edb651059d7e7db6269bc1c6ff518af61abb6507
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-11-26 21:28:42 +0100

    Step 1: Add AppBar and IconButton

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,26 @@
-void main(){
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(LocalisedApp());
+}
+
+class LocalisedApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          actions: <Widget>[
+            IconButton(
+                icon: Icon(
+              Icons.settings,
+              color: Colors.white,
+            ))
+          ],
+          title: Text("Localised App"),
+        ),
+        body: Container(),
+      ),
+    );
+  }
 }
