6ea551e95fd01ee5c880b864d6202f3660de0a2a
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-12-01 10:12:01 +0100

    Step 7: added AppTranslationsDelegate for loading selected locale

---
+++ lib/app_translations_delegate.dart
@@ -0,0 +1,25 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:in_app_localisation/app_translations.dart';
+
+class AppTranslationsDelegate extends LocalizationsDelegate<AppTranslations> {
+  final Locale newLocale;
+
+  const AppTranslationsDelegate({this.newLocale});
+
+  @override
+  bool isSupported(Locale locale) {
+    return ["en", "es"].contains(locale.languageCode);
+  }
+
+  @override
+  Future<AppTranslations> load(Locale locale) {
+    return AppTranslations.load(newLocale ?? locale);
+  }
+
+  @override
+  bool shouldReload(LocalizationsDelegate<AppTranslations> old) {
+    return true;
+  }
+}
